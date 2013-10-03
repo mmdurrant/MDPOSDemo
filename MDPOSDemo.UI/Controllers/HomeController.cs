a3193b455c356f1140f33215282413aa5dd69e6d
Michael Durrant <mmdurrant@gmail.com>
2013-10-03 15:33:44 -0600

    Added API script to layout
    Added LoginController
    Added BIZFactory
    Added HomeController that doesn't do much right now.

--- MDPOSDemo.UI/Controllers/HomeController.cs
+++ MDPOSDemo.UI/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MDPOSDemo.BIZ.Contracts;
 
 namespace MDPOSDemo.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private ILoginBiz _loginRequestsInterface;
+        public ILoginBiz LoginRequestsInterface
+        {
+            get { return _loginRequestsInterface ?? (_loginRequestsInterface = Factories.BIZFactory.GetLoginBiz()); }
+            set { _loginRequestsInterface = value; }
+        }
+
         //
         // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
---
+++ MDPOSDemo.UI/Factories/BIZFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MDPOSDemo.BIZ;
+using MDPOSDemo.BIZ.Contracts;
+
+namespace MDPOSDemo.UI.Factories
+{
+    public class BIZFactory
+    {
+        public static ILoginBiz GetLoginBiz()
+        {
+            return new LoginBiz();
+        }
+    }
+}
